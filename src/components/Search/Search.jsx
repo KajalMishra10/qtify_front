5270a289693e9f196bec7c1545a97c7b6629333e
Kajal <84652770+KajalMishra10@users.noreply.github.com>
2024-03-14 22:42:12 +0530

    Update Search.jsx

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,49 +1,16 @@
-import React, { useEffect,useState } from "react";
+import React from "react";
 import styles from "./Search.module.css";
-import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
-import { ReactComponent as SearchImage } from "../../assets/SearchImage.svg";
-import { fetchTopAlbums, fetchNewAlbums } from "../../api/api";
-
-const Search = () => {
-
-  const [albums, setAlbums] = useState([])
-
-  useEffect(()=>{
-    fetchAlbums()
-  },[])
-
-  const fetchAlbums = async() => {
-    const topAlbums = await fetchTopAlbums()
-    const newAlbums = await fetchNewAlbums()
-    // console.log([...topAlbums,...newAlbums])
-    setAlbums([...topAlbums,...newAlbums])
-  } 
-
+import { ReactComponent as SearchIcon } from "../../assets/Search_icon.svg";
+export default function Search({ placeholder }) {
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div className={styles.formWrapper}>
-      <Autocomplete
-        className={styles.search}
-        freeSolo
-        id={styles.autocomplete}
-        disableClearable
-        options={albums.map((album) => album.title)}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Search a album of your choice"
-            InputProps={{
-              ...params.InputProps,
-              type: "search",
-            }}
-          />
-        )}
-      />
-      <button className={styles.searchButton}>
-        <SearchImage />
+    <form className={styles.wrapper} onSubmit={onSubmit}>
+      <input className={styles.search} required placeholder={placeholder} />
+      <button className={styles.searchButton} type="submit">
+        <SearchIcon />
       </button>
-    </div>
+    </form>
   );
-};
-
-export default Search;
+}
